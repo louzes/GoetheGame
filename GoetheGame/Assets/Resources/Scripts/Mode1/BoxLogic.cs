9148d439aa7a919a236c5e4e357a278d08023a06
louzes <i.mpossible1551@gmail.com>
2023-06-20 16:02:02 +0500

    Кубик притягивается к ячейке
    исправили скрипт BoxLogic

--- GoetheGame/Assets/Resources/Scripts/Mode1/BoxLogic.cs
+++ GoetheGame/Assets/Resources/Scripts/Mode1/BoxLogic.cs
@@ -1,32 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BoxLogic : MonoBehaviour
-{
-    [SerializeField] public Transform CorrectBox;
-
-    [SerializeField] private float _minDistance;
-    [SerializeField] private float _speed;
-
-    [HideInInspector] private bool _vstavka;
+{ 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Correct")
         {
-            _vstavka = true;
+            transform.position = collision.transform.position;
             Debug.Log("ENTER");
-        }
-        else _vstavka = false;
-    }
-    private void FixedUpdate()
-    {
-        if (_vstavka == true)
-        {
-            if (Vector2.Distance(transform.position, CorrectBox.position) > _minDistance)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, CorrectBox.position, _speed * Time.deltaTime);
-            }
         }
     }
 }
