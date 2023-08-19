024e8a3011dde31166509262b57f6e89687908ba
louzes <i.mpossible1551@gmail.com>
2023-08-19 16:14:23 +0500

    Ошибка со слоями и звуки
    Исправлена ошибка со слоями и добавлены звуки клика (не до конца, только на кнопку Back в меню)

--- GoetheGame/Assets/Resources/Scripts/MainMenu/ButtonsLogic.cs
+++ GoetheGame/Assets/Resources/Scripts/MainMenu/ButtonsLogic.cs
@@ -4,6 +4,18 @@ using UnityEngine.EventSystems;
 
 public class ButtonsLogic : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] public AudioClip Click;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        audioSource.clip = Click;
+    }
     public void OnPointerEnter(PointerEventData eventData)
     {
         transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
@@ -12,5 +24,13 @@ public class ButtonsLogic : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     public void OnPointerExit(PointerEventData eventData)
     {
         transform.localScale = Vector3.one;
-    }    
+    }
+
+    public void AudioClick()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(Click);
+        }
+    }
 }
